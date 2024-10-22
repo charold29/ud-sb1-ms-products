edce4471371fc9268f096cadacdee7900f6a19f5
charoldev <harolrop14@gmail.com>
2024-10-21 20:15:29 -0500

    first controller

--- src/main/java/io/charoldev/msproducts/controller/ProductController.java
+++ src/main/java/io/charoldev/msproducts/controller/ProductController.java
@@ -1,11 +1,35 @@
 package io.charoldev.msproducts.controller;
 
+import io.charoldev.msproducts.model.Product;
 import io.charoldev.msproducts.service.ProductServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
+@RequiredArgsConstructor
 public class ProductController {
 
     private final ProductServiceImpl productService;
+
+    @GetMapping
+    public List<Product> list() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> details(@PathVariable Long id) {
+        Optional<Product> productOptional = productService.findById(id);
+
+        if (productOptional.isPresent()){
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
             
